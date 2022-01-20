2c9ca09133e7c39386599e04cae281ceedb99de2
Shantanu pawar <87530022+Shantanu-pawar@users.noreply.github.com>
2022-01-20 23:04:55 +0530

    updation

--- Interview_Preperation/CommonQues/Q10_Reverse_a_String.java
+++ Interview_Preperation/CommonQues/Q10_Reverse_a_String.java
@@ -3,14 +3,16 @@ package Interview_Preperation.CommonQues;
 public class Q10_Reverse_a_String {
     public static void main(String[] args) {
 
-        String name = "shantanu";
-        String rev = "" ;
-
-        for (int i = name.length()-1 ; i >= 0; i--) {
-            rev = rev + name.charAt(i);
-        }
-        System.out.println("Reverse of "+ name + " is " + rev);
-
-
-    }
+// 		System.out.println("GfG!");
+		
+		String name = "geeksforgeeks";
+		String rev = "";
+	
+		for(int i = 0 ; i < name.length()- 1; i++){
+		    rev += name.charAt(i);
+		    
+		}
+		System.out.println(rev + " is the reverse name of " + name);
+		
+	}
 }
